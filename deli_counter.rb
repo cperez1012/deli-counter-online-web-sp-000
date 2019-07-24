b257ac01c07b348807f64f34b8ef650f3a70c163
Christian Perez <perezchristian1012@gmail.com>
2019-07-24 21:42:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,19 @@
 # Write your code here.
 def line(katz_deli)
+  if katz_deli.empty?
 puts "The line is currently empty."
+else
+  current_line = "The line is currently:"
+  katz_deli.each_with_index do |name, index|
+    current_line << " #{index + 1}. #{name}"
+  end
+  puts current_line
 end
+end
+
+def take_a_number(katz_deli, name)
+  Counter = 1
+  
+end
+
+def now_serving
